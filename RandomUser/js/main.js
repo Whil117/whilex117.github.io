dbb2f4db3ab3a9d5febb92530ef22a8f146d4be5
WhiLEx <miltonivan117@gmail.com>
2021-04-14 18:44:34 -0600

    Api 2

---
+++ RandomUser/js/main.js
@@ -0,0 +1,27 @@
+const url = "https://jsonplaceholder.typicode.com/users",
+  root = document.getElementById("root"),
+  searchUser = document.getElementById("searchUser");
+
+let data;
+const showData = (items) =>
+  items
+    .map(
+      (item) =>
+        `<h1>${item.name}</h1> <p>${item.email}</p> <p>${item.address.street}</p> <p>${item.phone}</p>`
+    )
+    .join("");
+window.onload = async () => {
+  data = await fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => data)
+    .catch((err) => console.log(err));
+  root.innerHTML = showData(data);
+  console.log(data);
+};
+
+let search = "";
+searchUser.addEventListener("keyup", (e) => {
+  root.innerHTML = showData(
+    data.filter((item) => item.name.includes(e.target.value))
+  );
+});
