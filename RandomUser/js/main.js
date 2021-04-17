887d7173c037dc0d85b7d0320c4295f849374a25
WhiLEx <miltonivan117@gmail.com>
2021-04-17 11:40:08 -0600

    Project Improvements

--- RandomUser/js/main.js
+++ RandomUser/js/main.js
@@ -1,25 +1,51 @@
 const url = "https://jsonplaceholder.typicode.com/users",
   root = document.getElementById("root"),
   searchUser = document.getElementById("searchUser");
+const urlImgUser = "https://jsonplaceholder.typicode.com/photos";
 
+let dataUser;
+let dataImgUser;
 let data;
 const showData = (items) =>
   items
     .map(
       (item) =>
-        `<h1>${item.name}</h1> <p>${item.email}</p> <p>${item.address.street}</p> <p>${item.phone}</p>`
+        `
+        <div class="card">
+                  <div class="card__line"></div>
+                  
+                  <div class="card__item">
+                  <img class="card__item-img" src="${item.Img.url}"></img>
+                  
+                  <div class="card__item-infoUser">
+                  <h1 class="card__item-name">${item.name}</h1>
+                  <p class="card__item-address-item">City: ${item.address.city}</p>
+                  <p class="card__item-email">E-mail: ${item.email}</p> 
+                  <p class="card__item-phone">Phone: ${item.phone}</p>
+                  <p>Work-Site: ${item.company.name}</p>
+                  </div>
+                  </div>
+                  </div>
+                  `
     )
     .join("");
+
 window.onload = async () => {
-  data = await fetch(url)
+  dataImgUser = await fetchapp(urlImgUser);
+  dataUser = await fetchapp(url);
+  const newInfo = dataUser.map((user, indice) => {
+    const obj = { ...user, Img: dataImgUser[indice] };
+    return obj;
+  });
+  console.log(newInfo);
+  root.innerHTML = showData(newInfo);
+};
+const fetchapp = async (url) =>
+  await fetch(url)
     .then((Response) => Response.json())
     .then((data) => data)
     .catch((err) => console.log(err));
-  root.innerHTML = showData(data);
-  console.log(data);
-};
 
-let search = "";
 searchUser.addEventListener("keyup", (e) => {
   root.innerHTML = showData(
     data.filter((item) => item.name.includes(e.target.value))
