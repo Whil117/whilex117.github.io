b54bb0780532231c0563daa02a23b8e68aefb77b
WhiLEx <miltonivan117@gmail.com>
2021-04-19 15:26:25 -0600

    refactorize

--- RandomUser/js/main.js
+++ RandomUser/js/main.js
@@ -1,53 +1,55 @@
-const url = "https://jsonplaceholder.typicode.com/users",
-  root = document.getElementById("root"),
-  searchUser = document.getElementById("searchUser");
-const urlImgUser = "https://jsonplaceholder.typicode.com/photos";
+const rootDom = document.getElementById("root");
 
-let dataUser;
-let dataImgUser;
-let data;
-const showData = (items) =>
-  items
-    .map(
-      (item) =>
-        `
-        <div class="card">
-                  <div class="card__line"></div>
-                  
-                  <div class="card__item">
-                  <img class="card__item-img" src="${item.Img.url}"></img>
-                  
-                  <div class="card__item-infoUser">
-                  <h1 class="card__item-name">${item.name}</h1>
-                  <p class="card__item-address-item">City: ${item.address.city}</p>
-                  <p class="card__item-email">E-mail: ${item.email}</p> 
-                  <p class="card__item-phone">Phone: ${item.phone}</p>
-                  <p>Work-Site: ${item.company.name}</p>
-                  </div>
-                  </div>
-                  </div>
-                  `
-    )
-    .join("");
+const baseUrl = "https://jsonplaceholder.typicode.com/";
 
-window.onload = async () => {
-  dataImgUser = await fetchapp(urlImgUser);
-  dataUser = await fetchapp(url);
-  const newInfo = dataUser.map((user, indice) => {
-    const obj = { ...user, Img: dataImgUser[indice] };
-    return obj;
-  });
-  console.log(newInfo);
-  root.innerHTML = showData(newInfo);
+const map = (mapper) => {
+  return (array) => array.map(mapper);
 };
-const fetchapp = async (url) =>
+
+const filter = (predicate) => {
+  return (array) => array.filter(predicate);
+};
+
+const userShowData = map((item) => {
+  return `
+    <div class="card">
+      <div class="card__line"></div>
+      <div class="card__item">
+      <div>  
+         <img class="card__item-img" src="${item.Img.url}"></img></div>
+ 
+      
+          <div class="card__item-infoUser">
+          <h1 class="card__item-name">${item.name}</h1>
+          <p class="card__item-address-item">City: ${item.address.city}</p>
+          <p class="card__item-email">E-mail: ${item.email}</p> 
+          <p class="card__item-phone">Phone: ${item.phone}</p>
+          <p>Work-Site: ${item.company.name}</p>
+          </div>
+      </div>
+    </div> `;
+});
+
+const fetchIt = async (url) =>
   await fetch(url)
     .then((Response) => Response.json())
     .then((data) => data)
     .catch((err) => console.log(err));
 
-searchUser.addEventListener("keyup", (e) => {
-  root.innerHTML = showData(
-    data.filter((item) => item.name.includes(e.target.value))
-  );
-});
+const app = async () => {
+  const inputUserFind = document.getElementById("searchUser");
+  const userPhoto = await fetchIt(`${baseUrl}photos`);
+  const userInfo = await fetchIt(`${baseUrl}users`);
+  const user = userInfo.map((user, indice) => {
+    const obj = { ...user, Img: userPhoto[indice] };
+    return obj;
+  });
+
+  rootDom.innerHTML = userShowData(user).join("");
+
+  inputUserFind.addEventListener("keyup", (e) => {
+    const userFilter = filter((item) => item.name.includes(e.target.value));
+    rootDom.innerHTML = userShowData(userFilter(user));
+  });
+};
+app();
